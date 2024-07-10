9e7a166ee42bb957772c8d37841f5f877fae05dd
Muhammad Naufan <muhammadnaufan719@yahoo.com>
2024-07-10 15:40:45 +0700

    fix: tailwind library

--- src/config/Api.tsx
+++ src/config/Api.tsx
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 
 const axios = Axios.create({
-	baseURL: process.env.REACT_APP_ENDPOINT_API
+	baseURL: 'https://grim-sandi-naufan-a5e18060.koyeb.app/api'
 });
 
 export default axios;
